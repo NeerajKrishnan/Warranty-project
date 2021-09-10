6bd8d6c54cba2b7d98b87a9e526a45012dba063e
NeerajKrishnan <neerajkrishnan82@gmail.com>
2021-09-10 16:37:41 +0530

    [ADD] Initial commit

--- milestonetask/models/saleorder.py
+++ milestonetask/models/saleorder.py
@@ -9,8 +9,31 @@ class SaleOrder(models.Model):
     def action_create_project(self):
              project_template2 = self.env['project.project'].create({
             'name': self.name,
-            'active': True,  # this template is archived
-        })
-
+            'active': True
+             })
+             avoid_repeat = []
+             parent_task=[]
+             temp_sale_order=self.order_line
+             for i in temp_sale_order:
+                 if not (i.milestone in avoid_repeat):
+                     avoid_repeat.append(i.milestone)
+             for i in avoid_repeat:
+                 p_task =self.env['project.task'].create(
+                     {
+                         'name': 'Milestone - '+str(i),
+                         'project_id': project_template2.id
+                     }
+                 )
+                 parent_task.append([i,p_task.id])
+             for i in temp_sale_order:
+                 for k in parent_task:
+                     if i.milestone == k[0]:
+                        p_task = self.env['project.task'].create(
+                        {
+                         'name': 'Milestone - ' + str(i.milestone)+" "+i.product_id.name,
+                         'project_id': project_template2.id,
+                         'parent_id': k[1]
+                        }
+                      )
 
 
